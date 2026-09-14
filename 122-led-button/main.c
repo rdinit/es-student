4e64c7e5cbaa97b314ea2134cc3f44519910a92c
rdinit <28570294+rdinit@users.noreply.github.com>
2026-09-14 23:03:43 +0300

    hw 1.2

---
+++ 122-led-button/main.c
@@ -0,0 +1,39 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+const uint LED_PIN = 25;
+
+// добавляем заголовочный файл функций ввода-вывода
+// добавляем заголовочный файл функций работы с GPIO
+
+// объявляем константу вывода светодиода
+const uint BUTTON_PIN = 15;
+
+int main()
+{
+    // инициализируем пин светодиода
+    // настраиваем пин светодиода на выход
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+
+    bool led = false;
+    bool previous = false;
+
+    while (1)
+    {
+        bool current = gpio_get(BUTTON_PIN);
+
+        if (previous == true && current == false)
+        {
+            led = !led;
+            gpio_put(LED_PIN, led);
+        }
+
+        previous = current;
+    }
+}
